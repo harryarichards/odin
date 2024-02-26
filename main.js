d950e01573c008d4a7fbef5ddcd2ec68a8075c3c
harryarichards <harry_richards_@hotmail.co.uk>
2024-02-26 22:17:00 +0000

    implemented playGame function

--- main.js
+++ main.js
@@ -7,28 +7,57 @@ function getComputerChoice() {
 function playRound() {
     let userChoice = parseInt(prompt("Select your choice (0: Rock, 1: Paper, 2: Scissors)..."));
     let computerChoice = getComputerChoice();
+    console.log(userChoice);
     console.log(`You played ${CHOICE_MAPPING[userChoice]}.`);
     console.log(`Computer played ${CHOICE_MAPPING[computerChoice]}.`);
 
     if (userChoice === computerChoice) {
         console.log("Draw!");
+        return 0;
     }
     else if ((!userChoice) && (computerChoice===1)){
-        console.log("You lost!")
+        
+        return -1;
     }
     else if ((userChoice===1) && (computerChoice===2)){
-        console.log("You lost!")
+        return -1;
     }
     else if ((userChoice===2) && (!computerChoice)){
-        console.log("You lost!")
+        return -1;
     }
     else {
-        console.log("You won!")
+        return 1;
+    }
+}
+
+function playGame(){
+    console.log("--- NEW GAME ---")
+    let userScore = 0;
+    for (let i =0; i<5; i++){
+        let outcome = playRound();
+        if (outcome>0){
+            console.log("You won this round!")
+        }
+        else if (!outcome){
+            console.log("You drew round!")
+        }
+        else {
+            console.log("You lost this round!")
+        }
+        userScore += playRound();
+        console.log();
+    }
+
+    if (userScore>0){
+        console.log(`You scored ${userScore}, you won the game!`)
+    }
+    else if (!userScore){
+        console.log(`You scored ${userScore}, you drew the game!`)
+    }
+    else {
+        console.log(`You scored ${userScore}, you lost the game!`)
     }
     console.log();
 }
 
-
-while (true) {
-    playRound()
-}
+playGame()
