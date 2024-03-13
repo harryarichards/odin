7cfe8adc69d3148810e40978f9da14cc3768e54a
harryarichards <harry_richards_@hotmail.co.uk>
2024-03-13 00:37:42 +0000

    leapYears function implemented

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!(year%400)){
+        return true;
+    } else if (!(year%100)){
+        return false;
+    } else if (!(year%4)){
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
